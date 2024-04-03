f5d4b2cd5eb60e0dd9c8c34acb1dff8f50fe69b4
elisya10 <149879590+elisya10@users.noreply.github.com>
2024-04-03 13:58:32 +0800

    app.js
    update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Elisya Engka      52021004      Teknik Informatika')
 })
 
 app.listen(PORT, HOST)
